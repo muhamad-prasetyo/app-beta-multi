215b3e5d39dd1a9ecd4dbdb7e40b8b2f30d78012
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2021-06-15 18:02:18 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const tab = document.querySelectorAll(".tab");
+
+tab[0].checked = true;
+
+tab.forEach((e)=>{
+    e.addEventListener("click", (event)=>{
+        // console.log(tab);
+        tab.forEach((e)=>{
+            e.checked = false;
+            e.parentElement.classList.remove("active");
+        })
+        event.target.checked = true;
+            e.parentElement.classList.add("active");
+    })
+});
